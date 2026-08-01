dadda0e9b67007967fb828b3552990949d119263
TXW15055317277 <1351768609@qq.com>
2026-08-01 12:56:41 +0800

    UART改为中断模式

--- 6.STM32_I2C/Driver/USART.h
+++ 6.STM32_I2C/Driver/USART.h
@@ -4,11 +4,13 @@
 #include "stm32f4xx.h"
 #include <stdio.h>
 
+extern uint8_t buffer[100];
+extern uint8_t size;
+
 void USART_Init_(uint32_t Baudrate);
-uint8_t USART_Receive(void);
 void USART_Transmit(uint8_t message);
-void USART_String_rx(uint8_t buffer[], uint8_t *size);
 void USART_String_tx(uint8_t *str, uint8_t size);
-int fputc(int ch, FILE *file);
+void USART1_IRQHandler(void);
+int fputc(int ch, FILE * file);
 
 #endif
--- 7.STM32_I2C_hardware/Driver/USART.c
+++ 7.STM32_I2C_hardware/Driver/USART.c
@@ -11,7 +11,8 @@ void USART_Init_(uint32_t Baudrate)
 	DIV_F = (USARTDIV - DIV_M)*16+0.5f;
 	
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN ;
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+
 
 	GPIOA->AFR[1] |= (GPIO_AF_USART1 << 4);
 	GPIOA->AFR[1] |= (GPIO_AF_USART1 << 8);
@@ -31,17 +32,23 @@ void USART_Init_(uint32_t Baudrate)
 
 	USART1->BRR = DIV_M<<4 | DIV_F;
 	USART1->CR1 |= USART_CR1_TE | USART_CR1_RE;
+	USART1->CR1 |= USART_CR1_RXNEIE | USART_CR1_IDLEIE;
 	USART1->CR1 &= ~USART_CR1_M;
 	USART1->CR1 &= ~USART_CR1_PCE;
 	USART1->CR2 &= ~USART_CR2_STOP;
 	USART1->CR1 |= USART_CR1_UE;
-}
+	
+	//开启中断时钟
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	RCC->APB2ENR |= RCC_APB2ENR_EXTIEN;
 
-uint8_t USART_Receive(void)
-{
-    while ((USART1->SR & USART_SR_RXNE) == 0)
-    {}
-    return USART1->DR;
+	//设置分组，从011->100->101->110->111，分别为xxxx,xxx.y,xx.yy,x.yyy,yyyy
+	SCB->AIRCR &= ~SCB_AIRCR_PRIGROUP_Msk;
+	SCB->AIRCR |= 3U << 8;
+	//设置优先级1111
+	NVIC->IP[USART1_IRQn] |= 15 << 4;
+	//ISER寄存器为32位，因此将序号右移5位为除以32，确定在哪个寄存器，后面则是取低5位，将对应序号的使能位置1
+	NVIC->ISER[(((uint32_t)(int32_t)USART1_IRQn) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)(int32_t)USART1_IRQn) & 0x1FUL));
 }
 
 void USART_Transmit(uint8_t message)
@@ -49,24 +56,6 @@ void USART_Transmit(uint8_t message)
     while ((USART1->SR & USART_SR_TXE) == 0)
     {}
     USART1->DR = message;
-}
-
-void USART_String_rx(uint8_t buffer[], uint8_t *size)
-{
-	uint8_t i = 0;
-	while (1)
-	{
-		while ((USART1->SR & USART_SR_RXNE) == 0)
-		{
-			if ((USART1->SR & USART_SR_IDLE))
-			{
-				*size = i;
-				return;
-			}
-		}
-		buffer[i] = USART1->DR;
-		i++;
-	}
 }
 
 void USART_String_tx(uint8_t *str, uint8_t size)
@@ -78,7 +67,23 @@ void USART_String_tx(uint8_t *str, uint8_t size)
 	}
 }
 
-int fputc(int ch, FILE *file)
+void USART1_IRQHandler(void)
+{
+	if (USART1->SR & USART_SR_RXNE)
+	{
+		buffer[size] = USART1->DR;
+		size++;
+	}
+	else if (USART1->SR & USART_SR_IDLE)
+	{
+		USART1->SR;
+		USART1->DR;
+		USART_String_tx(buffer, size);
+		size = 0;
+	}
+}
+
+int fputc(int ch, FILE * file)
 {
 	USART_Transmit(ch);
 	return ch;
